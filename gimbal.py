23dadd9d5602f5a22daa3cbf014462b71a958a2c
Calvin Tarplee <cmtarplee@icloud.com>
2026-01-01 11:23:14 -0500

    added non-zervo hat gimbal control

---
+++ gimbal.py
@@ -0,0 +1,16 @@
+
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(12)
+# 12, 13, 19, 18
+
+while True:
+    servo.min()
+    sleep(1)
+    servo.mid()
+    sleep(1)
+    servo.max()
+    sleep(1)
+
+
